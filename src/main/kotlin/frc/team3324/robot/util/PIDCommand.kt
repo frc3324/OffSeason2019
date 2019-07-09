2c7d7147db3333f0422c4918bc0fa2ffabcf580a
Mani Kamali <kamalisarvestani.1@themetroschool.org>
2019-07-08 20:48:27 -0400

    added PID code

---
+++ src/main/kotlin/frc/team3324/robot/util/PIDCommand.kt
@@ -0,0 +1,53 @@
+package frc.team3324.robot.util
+
+import edu.wpi.first.wpilibj.Notifier
+import edu.wpi.first.wpilibj.command.Command
+import edu.wpi.first.wpilibj.command.Subsystem
+import java.util.function.DoubleConsumer
+import java.util.function.DoubleSupplier
+
+class PIDCommand(val kP: Double, val kI: Double, val kD: Double, val goal: Double, val dt: Double, val subsystem: Subsystem, val measurement: DoubleSupplier, val useOutput: DoubleConsumer): Command() {
+    protected var integral = 0.0
+    protected var lastPosition = 0.0
+    private val notifier = Notifier(this ::executePID)
+
+    init {
+        this.requires(subsystem)
+    }
+
+    override fun initialize() {
+       integral = 0.0
+       notifier.startPeriodic(dt)
+    }
+
+    fun executePID() {
+        val position = measurement.asDouble
+        val error = goal - position
+        val deriv = position - lastPosition
+
+        integral += error
+        lastPosition = position
+
+        val output = (error * kP) + (integral * kI) - (deriv * kD)
+        useOutput.accept(output)
+    }
+
+    override fun end() {
+        stopNotifier()
+    }
+
+    override fun interrupted() {
+        stopNotifier()
+        end()
+    }
+
+    fun stopNotifier() {
+        notifier.stop()
+        notifier.stop()
+    }
+
+    override fun isFinished(): Boolean {
+        return measurement.asDouble == goal
+    }
+
+}
